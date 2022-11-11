ab0bb0f3bddaba660690e417b352fdae39ba54fb
MSI <MSI@DESKTOP-SIG7LQ4>
2022-11-11 15:33:34 +0900

    각 기능별 폴더생성 uni.기능이름

--- src/com/uni/run/App.java
+++ src/com/uni/run/App.java
@@ -1,35 +1,35 @@
-package com.uni.run;
-
-import java.util.Scanner;
-
-public class App {
-
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		MenuController menuController = new MenuController();
-		
-		do {
-			System.out.println("====================막먹고 와 함께하는 건강한 식습관 GO GO GO====================");
-			System.out.println("1. 메뉴 전체 조회");
-			System.out.println("2. 메뉴 코드로 메뉴 조회");
-			System.out.println("3. 신규 메뉴 추가");
-			System.out.println("4. 메뉴 수정");
-			System.out.println("5. 메뉴 삭제");
-			System.out.print("메뉴 관리 번호를 입력하세요 : ");
-			int no = sc.nextInt();
-			
-			switch(no) {
-				case 1 : menuController.selectAllMenu(); break;
-				case 2 : menuController.selectMenuByCode(inputMenuCode()); break;
-				case 3 : menuController.registMenu(inputMenu()); break;  ///에러
-				case 4 : menuController.modifyMenu(inputModifyMenu()); break;
-				case 5 : menuController.deleteMenu(inputMenuCode()); break;
-				default : System.out.println("잘못된 메뉴를 선택하셨습니다."); break;
-			}
-			
-		} while(true);
-
-	}
-
-}
+//package com.uni.run;
+//
+//import java.util.Scanner;
+//
+//public class App {
+//
+//	public static void main(String[] args) {
+//		
+//		Scanner sc = new Scanner(System.in);
+//		MenuController menuController = new MenuController();
+//		
+//		do {
+//			System.out.println("====================막먹고 와 함께하는 건강한 식습관 GO GO GO====================");
+//			System.out.println("1. 메뉴 전체 조회");
+//			System.out.println("2. 메뉴 코드로 메뉴 조회");
+//			System.out.println("3. 신규 메뉴 추가");
+//			System.out.println("4. 메뉴 수정");
+//			System.out.println("5. 메뉴 삭제");
+//			System.out.print("메뉴 관리 번호를 입력하세요 : ");
+//			int no = sc.nextInt();
+//			
+////			switch(no) {
+////				case 1 : menuController.selectAllMenu(); break;
+////				case 2 : menuController.selectMenuByCode(inputMenuCode()); break;
+////				case 3 : menuController.registMenu(inputMenu()); break;  ///에러
+////				case 4 : menuController.modifyMenu(inputModifyMenu()); break;
+////				case 5 : menuController.deleteMenu(inputMenuCode()); break;
+////				default : System.out.println("잘못된 메뉴를 선택하셨습니다."); break;
+////			}
+//			
+//		} while(true);
+//
+//	}
+//
+//}
--- src/com/uni/run/MenuController.java
+++ src/com/uni/run/MenuController.java
@@ -1,98 +1,98 @@
-package com.uni.section01.xmlconfig;
-
-import java.util.List;
-import java.util.Map;
-
-public class MenuController {
-	
-	private final PrintResult printResult;
-	private final MenuService menuService;
-	
-	public MenuController() {
-		printResult = new PrintResult();
-		menuService = new MenuService();
-		
-	
-	}
-
-	public void selectAllMenu() {
-		List<MenuDTO> menuList = menuService.selectAllMenu();
-		
-		if(menuList != null) {
-			printResult.printMenuList(menuList);
-		} else {
-			printResult.printErrorMessage("selectList");
-		}
-
-	}
-
-	public void selectMenuByCode(Map<String, String> parameter) {
-		int code = Integer.parseInt(parameter.get("code"));
-		
-		MenuDTO menu = menuService.selectMenuByCode(code);
-		
-		if(menu != null) {
-			printResult.printMenu(menu);
-		} else {
-			printResult.printErrorMessage("selectOne");
-		}
-
-	}
-
-	public void registMenu(Map<String, String> parameter) {
-		
-		String name = parameter.get("name");
-		int price = Integer.parseInt(parameter.get("price"));
-		int categoryCode = Integer.parseInt(parameter.get("categoryCode"));
-		
-		MenuDTO menu = new MenuDTO();
-		menu.setName(name);
-		menu.setPrice(price);
-		menu.setCategoryCode(categoryCode);
-		
-		if(menuService.registMenu(menu)) {
-			printResult.printSuccessMessage("insert");
-		} else {
-			printResult.printErrorMessage("insert");
-		}
-
-		
-	}
-	
-	
-	public void modifyMenu(Map<String, String> parameter) {
-		
-		int code = Integer.parseInt(parameter.get("code"));
-		String name = parameter.get("name");
-		int price = Integer.parseInt(parameter.get("price"));
-		int categoryCode = Integer.parseInt(parameter.get("categoryCode"));
-		
-		MenuDTO menu = new MenuDTO();
-		menu.setCode(code);
-		menu.setName(name);
-		menu.setPrice(price);
-		menu.setCategoryCode(categoryCode);
-		
-		if(menuService.modifyMenu(menu)) {
-			printResult.printSuccessMessage("update");
-		} else {
-			printResult.printErrorMessage("update");
-		}
-	}
-
-	public void deleteMenu(Map<String, String> parameter) {
-		
-		int code = Integer.parseInt(parameter.get("code"));
-		
-		if(menuService.deleteMenu(code)) {
-			printResult.printSuccessMessage("delete");
-		} else {
-			printResult.printErrorMessage("delete");
-		}
-		
-	}
-
-	
-	
-	
-}
+//package com.uni.section01.xmlconfig;
+//
+//import java.util.List;
+//import java.util.Map;
+//
+//public class MenuController {
+//	
+//	private final PrintResult printResult;
+//	private final MenuService menuService;
+//	
+//	public MenuController() {
+//		printResult = new PrintResult();
+//		menuService = new MenuService();
+//		
+//	
+//	}
+//
+//	public void selectAllMenu() {
+//		List<MenuDTO> menuList = menuService.selectAllMenu();
+//		
+//		if(menuList != null) {
+//			printResult.printMenuList(menuList);
+//		} else {
+//			printResult.printErrorMessage("selectList");
+//		}
+//
+//	}
+//
+//	public void selectMenuByCode(Map<String, String> parameter) {
+//		int code = Integer.parseInt(parameter.get("code"));
+//		
+//		MenuDTO menu = menuService.selectMenuByCode(code);
+//		
+//		if(menu != null) {
+//			printResult.printMenu(menu);
+//		} else {
+//			printResult.printErrorMessage("selectOne");
+//		}
+//
+//	}
+//
+//	public void registMenu(Map<String, String> parameter) {
+//		
+//		String name = parameter.get("name");
+//		int price = Integer.parseInt(parameter.get("price"));
+//		int categoryCode = Integer.parseInt(parameter.get("categoryCode"));
+//		
+//		MenuDTO menu = new MenuDTO();
+//		menu.setName(name);
+//		menu.setPrice(price);
+//		menu.setCategoryCode(categoryCode);
+//		
+//		if(menuService.registMenu(menu)) {
+//			printResult.printSuccessMessage("insert");
+//		} else {
+//			printResult.printErrorMessage("insert");
+//		}
+//
+//		
+//	}
+//	
+//	
+//	public void modifyMenu(Map<String, String> parameter) {
+//		
+//		int code = Integer.parseInt(parameter.get("code"));
+//		String name = parameter.get("name");
+//		int price = Integer.parseInt(parameter.get("price"));
+//		int categoryCode = Integer.parseInt(parameter.get("categoryCode"));
+//		
+//		MenuDTO menu = new MenuDTO();
+//		menu.setCode(code);
+//		menu.setName(name);
+//		menu.setPrice(price);
+//		menu.setCategoryCode(categoryCode);
+//		
+//		if(menuService.modifyMenu(menu)) {
+//			printResult.printSuccessMessage("update");
+//		} else {
+//			printResult.printErrorMessage("update");
+//		}
+//	}
+//
+//	public void deleteMenu(Map<String, String> parameter) {
+//		
+//		int code = Integer.parseInt(parameter.get("code"));
+//		
+//		if(menuService.deleteMenu(code)) {
+//			printResult.printSuccessMessage("delete");
+//		} else {
+//			printResult.printErrorMessage("delete");
+//		}
+//		
+//	}
+//
+//	
+//	
+//	
+//}
