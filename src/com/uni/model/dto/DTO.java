3db8ec33aa048e06c37d5441f5fee2e42e41d9f1
leeyounghyun <leeyounghyun@localhost>
2022-11-12 19:58:38 +0900

    영현 브런치에 커밋

--- src/com/uni/model/dto/DTO.java
+++ src/com/uni/model/dto/DTO.java
@@ -1,5 +1,24 @@
 package com.uni.model.dto;
 
 public class DTO {
-
+	
+	
+	private	int food_id;
+	private	String food_name;
+	private	String rcp_pat;
+	private	int info_eng;
+	private	int info_na;
+	private	int info_car;
+	private	int info_pro;
+	private	int info_fat;
+	private	String manual01;
+	private	String manual02;
+	private	String manual03;
+	private	String manual04;
+	private	String manual05;
+	private	String manual06;
+	private	String ban_yn;
+	
+	
+	
 }
--- src/com/uni/run/App.java
+++ src/com/uni/run/App.java
@@ -1,48 +1,82 @@
 package com.uni.run;
 
+import java.util.Scanner;
+
+import com.uni.controller.Controller;
+
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("막먹고고고~");
-		System.out.println("안녕~~");
-		
+
+		Scanner sc = new Scanner(System.in);
+		Controller menuController = new Controller();
+
+		do {
+			System.out.println("=========== 관리자 관리 ===========");
+			System.out.println("1. 음식 추가");
+			System.out.println("2. 음식 조회");
+			System.out.println("3. 음식 수정");
+			System.out.println("4. 음식 삭제");
+			System.out.println("5. 음식 블랙리스트 관리");
+			System.out.println("0. 프로그램 종료");
+			System.out.print("메뉴관리 번호를 입력하세요 : ");
+			int no = sc.nextInt();
+
+			switch (no) {
+			case 1:
+				menuController.insertOne(inputFood());
+				break;
+			case 2:
+				menuController.selectFood(inputFoodName());
+				break;
+			case 3:
+				menuController.updateFood(inputUpdateFood());
+				break;
+			case 4:
+				menuController.deleteFood(inputFoodName());
+				break;
+			case 5:
+				menuController.banFood(banFood());
+				break;
+			case 6:
+				menuController.shotdown(shotdown());
+				break;
+			default:
+				System.out.println("잘못된 메뉴를 선택하셨습니다.");
+				break;
+			}
+
+		} while (true);
+
 	}
 
+	private static Object shotdown() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static Object banFood() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static Object inputUpdateFood() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static Object inputFoodName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static Object inputFood() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+	
+
 }
-
-//package com.uni.run;
-//
-//import java.util.Scanner;
-//
-//public class App {
-//
-//	public static void main(String[] args) {
-//		
-//		Scanner sc = new Scanner(System.in);
-//		MenuController menuController = new MenuController();
-//		
-//		do {
-//			System.out.println("====================막먹고 와 함께하는 건강한 식습관 GO GO GO====================");
-//			System.out.println("1. 메뉴 전체 조회");
-//			System.out.println("2. 메뉴 코드로 메뉴 조회");
-//			System.out.println("3. 신규 메뉴 추가");
-//			System.out.println("4. 메뉴 수정");
-//			System.out.println("5. 메뉴 삭제");
-//			System.out.print("메뉴 관리 번호를 입력하세요 : ");
-//			int no = sc.nextInt();
-//			
-////			switch(no) {
-////				case 1 : menuController.selectAllMenu(); break;
-////				case 2 : menuController.selectMenuByCode(inputMenuCode()); break;
-////				case 3 : menuController.registMenu(inputMenu()); break;  ///에러
-////				case 4 : menuController.modifyMenu(inputModifyMenu()); break;
-////				case 5 : menuController.deleteMenu(inputMenuCode()); break;
-////				default : System.out.println("잘못된 메뉴를 선택하셨습니다."); break;
-////			}
-//			
-//		} while(true);
-//
-//	}
-//
-//}
-
