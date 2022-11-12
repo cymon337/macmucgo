371b0849edcecec6b2b1f1c5a88cf7fea62a1cb4
BuildTools <unconfigured@null.spigotmc.org>
2022-11-12 22:06:01 +0900

    mapper 세분화

--- src/com/uni/model/dto/FavoriteDTO.java
+++ src/com/uni/model/dto/FavoriteDTO.java
@@ -1,5 +1,31 @@
 package com.uni.model.dto;
 
 public class FavoriteDTO {
-
+	private int favId;
+	private int userId;
+	private int[] favFoodList;
+	public FavoriteDTO(int favId, int userId, int[] favFoodList) {
+		super();
+		this.favId = favId;
+		this.userId = userId;
+		this.favFoodList = favFoodList.clone();
+	}
+	public int getFavId() {
+		return favId;
+	}
+	public void setFavId(int favId) {
+		this.favId = favId;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int[] getFavFoodList() {
+		return favFoodList;
+	}
+	public void setFavFoodList(int[] favFoodList) {
+		this.favFoodList = favFoodList;
+	}
 }
